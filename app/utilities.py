5319323cfc9215b8090846fd8232e237047eac05
Daniil Kulbatski <daniil.kulbackiy@gmail.com>
2024-01-14 14:41:24 +0100

    Create utilities module

---
+++ app/utilities.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from typing import TypeAlias
+
+
+Fahrenheit: TypeAlias = float
+Celsius: TypeAlias = float
+
+
+class Utilities:
+    
+    @staticmethod
+    def get_date() -> datetime:
+        return datetime.now()
+
+    @staticmethod
+    def date_to_str(datetime: datetime, date_format: str) -> str:
+        return datetime.strftime(date_format)
+
+    @staticmethod
+    def fahrenheit_to_celsius(temperature: Fahrenheit) -> Celsius:
+        return (temperature - 32) * 5 / 9

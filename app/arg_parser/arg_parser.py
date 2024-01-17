158b7e5060830176beff0e85fdd950a744c3663b
Daniil Kulbatski <daniil.kulbackiy@gmail.com>
2024-01-17 22:15:26 +0100

    prepare script for new functionality

---
+++ app/arg_parser/arg_parser.py
@@ -0,0 +1,21 @@
+import argparse
+from enum import Enum
+
+from app import config
+
+
+class Command(str, Enum):
+    UPDATE_SST_DATABASE = "updatesstdb"
+    NO_COMMAND = ""
+
+
+class ArgParser:
+
+    def __init__(self) -> None:
+        self._parser = argparse.ArgumentParser(
+                prog=config.APP_NAME,
+                description=config.APP_DESCRIPTION)
+        self._parser.add_argument("command", nargs="?", default="")
+
+    def get_args(self) -> argparse.Namespace:
+        return self._parser.parse_args()

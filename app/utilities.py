77f716f72fbdfa37a19433a0dda0e439c4feece5
Daniil Kulbatski <daniil.kulbackiy@gmail.com>
2024-02-01 18:51:42 +0100

    add methods for string comaprison

--- app/utilities.py
+++ app/utilities.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from typing import TypeAlias
+from unicodedata import normalize
 
 
 Fahrenheit: TypeAlias = float
@@ -19,3 +20,12 @@ class Utilities:
     @staticmethod
     def fahrenheit_to_celsius(temperature: Fahrenheit) -> Celsius:
         return (temperature - 32) * 5 / 9
+
+    @staticmethod
+    def nfc_equal(str1: str, str2: str) -> bool:
+        return normalize("NFC", str1) == normalize("NFC", str2)
+
+    @staticmethod
+    def fold_equal(str1: str, str2: str) -> bool:
+        return (normalize("NFC", str1).casefold() ==
+                normalize("NFC", str2).casefold())

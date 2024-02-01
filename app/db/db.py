a435fcfce01e766bf2735edb00b4d84ab972b3ac
Daniil Kulbatski <daniil.kulbackiy@gmail.com>
2024-02-01 18:50:00 +0100

    add populate sst table logic

--- app/db/db.py
+++ app/db/db.py
@@ -1,13 +1,15 @@
-from sqlalchemy import select
+from typing import TypeAlias
+
+from sqlalchemy import delete, select
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import NoResultFound
 from sqlalchemy.exc import MultipleResultsFound
-from typing import TypeAlias
 
 from db_config import engine
 from models import Base, SeaSurfaceTemperature
 from app.exceptions import NoResultError
 from app.exceptions import MultipleResultsError
+from app.web_pserser.parser import SST
 
 
 Hyperlink: TypeAlias = str
@@ -17,7 +19,7 @@ class DB:
     def __init__(self) -> None:
        Base.metadata.create_all(engine)
 
-    def get_sst(self, country: str, city: str) -> Hyperlink:
+    def get_sst_hyperlink(self, country: str, city: str) -> Hyperlink:
         stmt = (
             select(SeaSurfaceTemperature.hyperlink)
             .where(SeaSurfaceTemperature.country == country)
@@ -30,3 +32,14 @@ class DB:
                 raise NoResultError(f"No sst for {country}, {city}.")
             except MultipleResultsFound:
                 raise MultipleResultsError(f"Multiple sst for {country}, {city} in database.")
+
+    def populate_sst_table(self, sst_list: list[SST]) -> None:
+        stmt = delete(SeaSurfaceTemperature)
+        sst_models_list = [SeaSurfaceTemperature(country=sst.country,
+                                                 city=sst.city,
+                                                 hyperlink=sst.href)
+                           for sst in sst_list]
+        with Session(engine) as session:
+            session.execute(stmt)
+            session.add_all(sst_models_list)
+            session.commit()
